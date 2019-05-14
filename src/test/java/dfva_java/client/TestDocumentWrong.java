3ea526151e80a7096bf1658fd9cf62a91acd6495
Miguel Mejias <miguel.mejias@ivrtechnology.com>
2019-05-14 13:23:30 -0600

    adds test for document wrong

---
+++ src/test/java/dfva_java/client/TestDocumentWrong.java
@@ -0,0 +1,85 @@
+package dfva_java.client;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.json.JsonObject;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+public class TestDocumentWrong {
+	public static List<String> DOCUMENT_ALLOWED_TEST = Arrays.asList();
+	private BaseUtils utils = BaseUtils.getInstance();
+
+	public static void save_obj(String identification,
+								String format, JsonObject obj,
+								BaseUtils utils){
+
+		Dictionary<String, JsonObject>  dicobj = utils.DOCUMENT_TRANSACTIONS.get(identification);
+		if(dicobj == null){
+			dicobj = new Hashtable<String, JsonObject> ();
+			utils.DOCUMENT_TRANSACTIONS.put(identification, dicobj);
+		}
+		dicobj.put(format, obj);
+	}
+
+	private Integer get_response(String format, String identification, String key){
+		Dictionary<String, JsonObject>  dicobj = utils.DOCUMENT_TRANSACTIONS.get(identification);
+
+		JsonObject obj = dicobj.get(format);
+		return obj.getInt(key);
+	}
+
+	@BeforeClass
+	public static void onceExecutedBeforeAll() {
+		JsonObject obj;
+		BaseUtils utils = BaseUtils.getInstance();
+		Enumeration<String> keys = utils.DOCUMENT_RESPONSE_WRONG_TABLE.keys();
+
+		String identification;
+		while(keys.hasMoreElements()){
+			identification = (String)keys.nextElement();
+			if( DOCUMENT_ALLOWED_TEST.isEmpty() || 	DOCUMENT_ALLOWED_TEST.indexOf(identification)!=-1){
+				for(String format: utils.DOCUMENT_FORMATS) {
+					obj = utils.client.sign(identification,
+							utils.read_files_inputstream("xml", "", "no_contrafirmado."),
+							format, //xml_cofirma, xml_contrafirma, odf, msoffice, pdf
+							"Sign document with format " + format
+					);
+					save_obj(identification, format, obj, utils);
+				}
+			}
+		}
+		try {
+			Thread.sleep(BaseUtils.WAIT_AUTH);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void do_checks(String format, String identification) {
+		Integer idtransaction = this.get_response(format, identification, "id_transaction");
+		JsonObject resobj = utils.client.sign_check(String.format("%d",idtransaction));
+
+		Integer expdata =utils.DOCUMENT_RESPONSE_WRONG_TABLE.get(identification);
+		assertEquals((Integer) resobj.getInt("status"), expdata);
+
+		Boolean ok = utils.client.sign_delete(String.format("%d",idtransaction));
+		assertEquals(ok, true);
+	}
+
+	@Test
+	public void test_contrafirma_not_ok(){
+		this.do_checks("xml_contrafirma", "03-0110-2020");
+	}
+}
+
